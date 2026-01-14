7f615fd3df3938e1c8927c417e266bfeb90a1ba7
jellybeannugget <yusefa2344@gmail.com>
2026-01-14 12:45:26 -0500

    practice 1

---
+++ Practice1.java
@@ -0,0 +1,10 @@
+public class Practice1{
+    public static void main(String[] args){
+        int [] primeNums = {2, 3, 5, 7, 11, 13, 17, 19};
+        System.out.println ("Array length: " + primeNums.length);
+        String [] friends = {"Suhaib", "Nkosi", "Muhammed"};
+        for(int i = 0; i <= friends.length-1; i++){
+            System.out.println(friends[i]);
+        }
+    }
+}
